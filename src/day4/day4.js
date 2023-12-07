0894118f6038028fcef9fa4d49f1da1b665bd192
Chris Menezes <chris@menezes.ca>
2023-12-06 21:48:58 -0500

    i said, the real day 4

--- src/day4/day4.js
+++ src/day4/day4.js
@@ -16,16 +16,17 @@ const parsed = fileInput
     .map(row => row.split(':'))
     .map(row => {
         const [card, numbers] = row;
-        const [cardName, cardNumber] = card.split(' ');
+        const [, cardNumber] = card.trim().split(/ +/);
         const numbersList = numbers
-            .split('|');           
-        
+            .split('|');
+
         const winningNumbers = numbersList[0];
         const yourNumbers = numbersList[1];
 
         function parseNums(x) {
             return x.trim().split(/ +/)
-            .map(it => parseInt(it.trim()))}
+                .map(it => parseInt(it.trim()))
+        }
         ;
 
         return {
@@ -53,85 +54,99 @@ const score1 = parsed.reduce((acc, item) => {
 
 console.log(`Score 1: ${score1}`);
 
-const cardsWithMatches = parsed.map(item => {
+const cardsWithMatches = parsed.map((item, index) => {
     const winningSet = new Set(item.winningNumbers);
     const yourSet = new Set(item.yourNumbers);
     const numMatches = new Set([...winningSet].filter(x => yourSet.has(x))).size;
 
     return {
         ...item,
+        index,
         numMatches
     };
 });
 
-function buildNewCardList(cardsWithMatches) {
-    
-    const withIndex = cardsWithMatches.map((item, index) => {
-        return {
-            ...item,
-            index
-        }
-    });
+function countCards(cardsWithMatches) {
 
-    const queue = withIndex.map(item => item);
+    let totalCount = 0;
 
-    const newCards = [];
-    let iterations = 0;
+    const numberOfCardsFromCard = {};
 
-    while(queue.length > 0) {
+    // initialize map
+    for (let i = 0; i < cardsWithMatches.length; i++) {
         
-        iterations++;
+        let { numMatches, cardNumber, index } = cardsWithMatches[i];
 
-        const it = queue.shift();
-        newCards.push(it);
-        const {index, numMatches} = it;
+        const childCards = [];
 
-        if (numMatches === 0) {
-            console.log(`Iterations: ${iterations}`)
-            continue
+        for (let j = 0; j < numMatches; j++) {
+            let newIndex = 1 + index + j;
+            childCards.push(cardsWithMatches[newIndex].cardNumber );
+        }
+        numberOfCardsFromCard[cardNumber] = childCards;
+    }
+
+    function recursiveCount(cardIndex) {
+        const children = numberOfCardsFromCard[cardIndex]
+        
+        if (children.length === 0) {
+            return cardIndex;
+        }
+
+        const toSum = []
+        for (let c of children) {
+            toSum.push(recursiveCount(c));
+        }
+        console.log(`-Card ${cardIndex} has ${toSum.flat()} cards`)
+        return [cardIndex, ...toSum.flat()];
+    }
+
+    for (let i = 0; i < cardsWithMatches.length; i++) {
+        const { cardNumber } = cardsWithMatches[i];
+        
+        const cnt = recursiveCount(cardNumber);
+        totalCount += cnt.length;
+        console.log(`Card ${cardNumber} has ${cnt} cards`)
+    }
+
+    return totalCount
+}
+
+function countCards2(cardsWithMatches) {
+    let countMap = {};
+
+    // initialize map
+    for (let i = 0; i < cardsWithMatches.length; i++) {
+        let { numMatches, cardNumber, index } = cardsWithMatches[i];
+        console.log(`processing cardNumber ${cardNumber}, numMatches ${numMatches}`)
+        if(cardNumber in countMap) {
+            countMap[cardNumber]++;
         } else {
-            
-            const toAdd = [];
-            for (let i = 0; i < numMatches; i++) {
-                toAdd.push(withIndex[index + i + 1]);
-            }
-
-            if (toAdd.length > 0){
-                queue.push(...toAdd);
-            }
-        };   
-    }
-    return newCards;
-}
-
-function buildNewCardList2(cardsWithMatches) {
-
-    const withIndex = cardsWithMatches.map((item, index) => {
-        return {
-            ...item,
-            index
+            countMap[cardNumber] = 1;
         }
-    });
-
-    const newCards = [];
-    const queue = [...withIndex];
-
-    while(queue.length > 0) {
-        const card = queue.shift();
-        newCards.push(card);
-
-        console.log(`queue size: ${queue.length}`);
         
-        if (card.numMatches > 0) {
-            const nextCards = withIndex.slice(card.index + 1, card.index + 1 + card.numMatches);
-            queue.push(...nextCards);
-
-            // console.log(`queue: ${queue.map(item => item.cardNumber)}`)
+        const indices = [];
+        for (let j = 0; j < numMatches; j++) {
+            let newIndex = 1 + parseInt(cardNumber) + j;
+            
+            indices.push(`${newIndex}`);
         }
-    }
+        console.log(`index ${cardNumber}, newIndices ${indices}`);
+        indices.forEach(i => {
+            if (i in countMap) {
+                countMap[i]+= countMap[cardNumber];
+            } else {
+                countMap[i] = countMap[cardNumber];
+            }
+        })
 
-    return newCards;
+        // console.log(`processing cardNumber ${cardNumber}`,countMap)
+    }
+    return countMap;
 }
 
-
-console.log(buildNewCardList2(cardsWithMatches).length);
+const countMap = countCards2(cardsWithMatches);
+const total = Object.values(countMap).reduce((a, b) => a + b, 0);
+console.log(`========================`);
+console.log(countMap)
+console.log(`total ${total}`);
